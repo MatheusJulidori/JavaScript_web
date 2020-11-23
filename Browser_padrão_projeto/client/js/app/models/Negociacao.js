58421f69a25436d0151b3426de1bcdd67b8e82d4
Matheus Henrique Julidori <matheus_julidori@yahoo.com.br>
2020-11-23 16:33:07 -0300

    Criando modelo de negociação

---
+++ Browser_padrão_projeto/client/js/app/models/Negociacao.js
@@ -0,0 +1,28 @@
+class Negociacao {
+
+    constructor(data, quantidade, valor) {
+        this._data = new Date(data.getTime());
+        this._quantidade = quantidade;
+        this._valor = valor;
+        this._volume = valor * quantidade;
+        Object.freeze(this);
+    }
+
+    get quantidade() {
+        return this._quantidade;
+    }
+
+    get data() {
+        data = new Date(this._data.getTime());
+        return data;
+    }
+
+    get valor() {
+        return this._valor;
+    }
+
+    get volume() {
+        return this._volume;
+    }
+
+}
