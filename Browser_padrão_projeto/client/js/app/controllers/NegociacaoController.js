ff2d9c76bbd7a7fbe7bfaa37993a9a012dbf3949
Matheus Henrique Julidori <matheus_julidori@yahoo.com.br>
2020-11-24 19:02:30 -0300

    Controller negociacao

---
+++ Browser_padrão_projeto/client/js/app/controllers/NegociacaoController.js
@@ -0,0 +1,30 @@
+class NegociacaoController {
+
+    constructor() {
+
+        let $ = document.querySelector.bind(document);
+
+        this._inputData = $('#data');
+        this._inputQuantidade = $('#quantidade');
+        this._inputValor = $('#valor');
+
+    }
+
+    adiciona(event) {
+        event.preventDefault();
+        alert("Negociação criada")
+
+        let data = new Date(this._inputData.value.split('-'));
+
+        let negociacao = new Negociacao(
+            data,
+            this._inputQuantidade.value,
+            this._inputValor.value
+        )
+
+        console.log(negociacao)
+    }
+
+
+
+}
